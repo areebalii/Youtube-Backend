2e85d23dc1878df67dd2217a55ba8903ae87fea4
Areeb Ali <areebktk8@gmail.com>
2026-07-03 18:08:07 +0500

    feat: add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({path: "./.env"});
 
 
 
 connectDB()
-
+  .then(() => {
+    app.listen(process.env.PORT || 9000, () => {
+      console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+    });
+  })
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process with an error code
+  });
 
 
 

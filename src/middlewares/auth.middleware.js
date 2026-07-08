39f068b862a6d15d353c068550562927a58d3ee2
Areeb Ali <areebktk8@gmail.com>
2026-07-08 17:44:12 +0500

    feat: implement logout functionality and update JWT verification error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,19 +5,23 @@ import { User } from "../models/user.model.js";
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => { 
-  const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer ", "")
-  if(!token) {
-    throw new ApiError(401, "Unauthorized: No token provided")
+  try {
+    const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer ", "")
+    if(!token) {
+      throw new ApiError(401, "Unauthorized: No token provided")
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+  
+    if(!user) {
+      throw new ApiError(401, "invalid access token")
+    }
+  
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid access token")
   }
-
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-
-  const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-
-  if(!user) {
-    throw new ApiError(401, "invalid access token")
-  }
-
-  req.user = user;
-  next();
 })

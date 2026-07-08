49b47d05df74a354c576bbe92ad2b9001ebcc8ee
Areeb Ali <areebktk8@gmail.com>
2026-07-08 11:47:21 +0500

    feat: enhance user registration process and improve cloudinary upload handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,16 +5,6 @@ import { User } from '../models/user.model.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 
 export const registerUser = asyncHandler(async (req, res) => {
-  // take the user data from the request body
-  // validate the user data
-  // check if the user already exists in the database
-  // if user exists, return an error response
-  // check for image and avatar
-  // upload the image to cloudinary using the uploadOnCloudinary function from src/utils/cloudinary.js
-  // create userObject with the user data and the image url from cloudinary
-  // save the userObject to the database
-  // remove the password from the userObject before sending the response
-  // check user creation and return the response with the user data and a success message
 
   const { fullName, email, username, password } = req.body
   console.log(email)
@@ -31,8 +21,8 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username already exists")
   }
 
-  const avatarLocalPath = req.files?.avatar[0]?.path
-  const coverImageLocalPath = req.files?.coverImage[0]?.path
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required")
@@ -42,7 +32,7 @@ export const registerUser = asyncHandler(async (req, res) => {
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
   if (!avatar) {
-    throw new ApiError(400, "avatar file is required ")
+    throw new ApiError(400, "avatar is required ")
   }
 
   const user = await User.create({
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import dotenv from "dotenv";
+import 'dotenv/config';
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-
-dotenv.config({path: "./.env"});
-
-
 
 connectDB()
   .then(() => {
@@ -17,43 +13,3 @@ connectDB()
     console.error("Error connecting to MongoDB:", err);
     process.exit(1); // Exit the process with an error code
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ; (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
-//     app.on("error", (err) => {
-//       console.log("Error connecting to MongoDB:", err);
-//       throw err;
-//     })
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server is running on port http://localhost:${process.env.PORT}`);
-//     });
-
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     throw error;
-//   }
-// })()

6125433724713854ae4769403c471b2bb3704093
Areeb Ali <areebktk8@gmail.com>
2026-07-11 11:12:49 +0500

    feat: add getUserChanelProfile function to fetch channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -324,3 +324,75 @@ export const updateUserCoverImage = asyncHandler(async (req, res) => {
     )
 
 })
+
+const getUserChanelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing")
+  }
+
+  const chanel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "Subscription",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "Subscription",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelSubscribedToCount: 1,
+        isSubscribed: 1
+      }
+    }
+  ])
+
+  if (!chanel?.length) {
+    throw new ApiError(404, "Channel does not exist")
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, chanel[0], "user channel fetched successfully")
+    )
+
+})

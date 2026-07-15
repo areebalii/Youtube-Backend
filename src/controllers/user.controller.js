8b743880094e0e4ab6ae9be131db890b2470ab63
Areeb Ali <areebktk8@gmail.com>
2026-07-15 19:00:15 +0500

    feat: export getUserChanelProfile and add getWatchHistory function to fetch user's watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -325,7 +325,7 @@ export const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 })
 
-const getUserChanelProfile = asyncHandler(async (req, res) => {
+export const getUserChanelProfile = asyncHandler(async (req, res) => {
   const { username } = req.params
 
   if (!username?.trim()) {
@@ -393,6 +393,60 @@ const getUserChanelProfile = asyncHandler(async (req, res) => {
     .status(200)
     .json(
       new ApiResponse(200, chanel[0], "user channel fetched successfully")
+    )
+
+})
+
+export const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.objectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully")
     )
 
 })

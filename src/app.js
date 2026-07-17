380cc4d45b48b054da8b1d3c92e12f1fff503700
Areeb Ali <areebktk8@gmail.com>
2026-07-17 21:05:25 +0500

    feat: add routes and controllers for tweets, likes, subscriptions, comments, videos, and playlists

--- src/app.js
+++ src/app.js
@@ -15,8 +15,20 @@ app.use(cookieParser());
 
 // Routes import
 import userRoutes from "./routes/user.routes.js";
+import tweetRoutes from "./routes/tweet.routes.js";
+import likeRoutes from "./routes/like.routes.js";
+import subscriptionRoutes from "./routes/subscription.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+import videosRoutes from "./routes/video.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 // Routes
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/tweets", tweetRoutes);
+app.use("/api/v1/likes", likeRoutes);
+app.use("/api/v1/subscriptions", subscriptionRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/videos", videosRoutes);
+app.use("/api/v1/playlist", playlistRoutes);
 
 export { app };
